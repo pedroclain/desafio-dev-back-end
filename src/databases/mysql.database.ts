fce8c75a705a7fec953e711ea969ee86a64b7277
Pedro Henrique Clain <pedroclain20@gmail.com>
2022-06-05 19:34:58 -0300

    Update mysql.database.ts

--- src/databases/mysql.database.ts
+++ src/databases/mysql.database.ts
@@ -4,9 +4,9 @@ const connection: Knex = knex({
     client: 'mysql2',
     connection: {
         host: '127.0.0.1',
-        user: 'root',
-        password: 'root',
-        database: 'test_knex',
+        user: 'seu usuario',
+        password: 'sua senha',
+        database: 'seu banco',
         requestTimeout: Infinity
     },
     useNullAsDefault: true,
